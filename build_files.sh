33bf289341eebce8052b6851bb24484542c7ed24
Suyashh Agarwal <110186843+SUYASHHAGARWAL@users.noreply.github.com>
2023-07-12 21:03:29 +0530

    jbjjkn

--- build_files.sh
+++
@@ -1,4 +0,0 @@
-echo "BUILS START"
-python3.9  -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --oinput --clear
-echo "BUILD COMPLETE"
--- djpaynrentapp/settings.py
+++ djpaynrentapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v$dfgg-5qoampx8^huqw%k(h5bgndcp3ok9z(+&c(qi=va!!6i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = []
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
 )
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 import os 
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'build_files','static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
